d2e4819810b1fffef85f67a1ca4ae1d3f7abd420
Evan Werner <evan.g.werner@gmail.com>
2021-10-01 13:42:48 -0400

    completed voters routes, insomnia testing successful on all routes

--- server.js
+++ server.js
@@ -1,70 +1,23 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001
+const db = require('./db/database');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-const inputCheck = require('./utils/inputCheck');
-const db = require('./db/database');
 const apiRoutes = require('./routes/apiRoutes');
-app.use('./apiRoutes', apiRoutes);
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Get all parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// Get single party by id
-app.get('/api/parties/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, rows) => {
-        if(err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// Delete a party
-app.delete('/api/parties/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(500).json({ error: res.message });
-        }
-        
-        res.json({
-            message: 'successfully deleted',
-            changes: this.changes
-        });
-    });
-});
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
 /*
 // Default response for any other request(Not Found) - Catch all
 app.use((req, res) => {
     res.status(404).end();
   }); */
-
 
 // Start server after DB connection
 db.on('open', () => {
